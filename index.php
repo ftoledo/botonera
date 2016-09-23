f765d3c87c93fc225ee47474bbb39f45119adc08
Fernando Toledo <ragnarok@docksud.com.ar>
2016-09-23 13:00:35 -0300

    agregando categorias

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
     text-shadow: 1px 1px 1px #666666;
     font-family: Arial;
     color: #ffffff;
-    font-size: 22px;
+    font-size: 18px;
     padding: 6px 16px 10px 16px;
     text-decoration: none;
     margin-bottom: 5px;
@@ -35,41 +35,68 @@
     input[type="submit"]::-moz-focus-inner {
      border: 1px dotted transparent;
     }
+    p.titulo {
+      margin: 0px;
+    }
+    /* Cloud */ 
+    hr.style-two { border: 0; height: 1px; background-image: linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0)); }
+
     </style>
-    
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-    <?php 
-    $cnt_a = 0;
-    $cnt_b = 0;
-    $dir  ='sonidos';
-    $archivos = array();
-    if ($dh = opendir($dir)) {
-        while (($file = readdir($dh)) !== false) {
-            if (is_file($dir.DIRECTORY_SEPARATOR.$file)) {
-                array_push($archivos, $file);
-            }
-        }
-        closedir($dh);
-    }
-    else {
-        echo "cannot open dir $dir";
-    }
-    natcasesort($archivos);
-    ?>
+ <?php 
+
+	function dirToArray($dir) {
+
+	   $result = array();
+
+	   $cdir = scandir($dir);
+	   foreach ($cdir as $key => $value)
+	   {
+		  if (!in_array($value,array(".","..")))
+		  {
+			 if (is_dir($dir . DIRECTORY_SEPARATOR . $value))
+			 {
+				$result[$value] = dirToArray($dir . DIRECTORY_SEPARATOR . $value);
+			 }
+			 else
+			 {
+				$result[] = $value;
+			 }
+		  }
+	   }
+
+	   return $result;
+	} 
+	$cnt_a = 0;
+	$cnt_b = 0;
+	$sounds_dir  ='sonidos';
+	$archivos = dirToArray($sounds_dir);
+?>
 </head>
 
 <body>
+<?php
+?>
     <h1>Botonera</h1>
-    <?php foreach ($archivos as $file):?>
-    <audio id='sonido<?php echo $cnt_a?>' src="<?php echo $dir.DIRECTORY_SEPARATOR.$file?>" preload="true">Tu navegador no soporta el elemento <code>audio</code>.</audio>
-    <?php $cnt_a++?>
-    <?php endforeach;?>
-    <?php foreach ($archivos as $file):?>
-    <?php $parts = pathinfo($file)?>
-    <button onclick="document.getElementById('sonido<?php echo $cnt_b?>').play();"><?php echo preg_replace("/[-_]/i"," ",$parts['filename'])?></button>
-    <?php $cnt_b++?>
-    <?php endforeach;?>
-    <a href="https://github.com/ftoledo/botonera"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></a>
+    <?php foreach ($archivos as $dir => $files):?>
+    <?php natcasesort($files)?>
+		<p class="titulo"><?php echo preg_replace("/[-_]/i"," ",$dir) ?></p>
+		<hr class="style-two"/> 
+
+		<?php foreach ($files as $file): ?>
+			<audio id='sonido<?php echo $cnt_a?>' src="<?php echo $sounds_dir.DIRECTORY_SEPARATOR.$dir.DIRECTORY_SEPARATOR.$file?>" preload="true">Tu navegador no soporta el elemento <code>audio</code>.</audio>
+			<?php $cnt_a++?>
+		<?php endforeach;?>
+		<?php foreach ($files as $file):?>
+			<?php $parts = pathinfo($file)?>
+			<button onclick="document.getElementById('sonido<?php echo $cnt_b?>').play();"><?php echo preg_replace("/[-_]/i"," ",$parts['filename'])?></button>
+			<?php $cnt_b++?>
+		<?php endforeach;?>
+
+	<?php endforeach;?>
+	<a href="https://github.com/ftoledo/botonera"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></a>
+
+  
 </body>
