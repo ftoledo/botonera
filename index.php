28b0785dc0396c96f3a99882adf78d0309f7d318
Diego Accorinti <diegoacco@gmail.com>
2016-10-27 15:04:21 -0300

    cambios CSS

--- index.php
+++ index.php
@@ -20,7 +20,8 @@
     font-family: Arial;
     color: #ffffff;
     font-size: 18px;
-    padding: 6px 16px 10px 16px;
+	padding: 5px 12px;
+    /* padding: 6px 16px 10px 16px; */
     text-decoration: none;
     margin-bottom: 5px;
     margin-right: 0px;
@@ -37,6 +38,8 @@
     }
     p.titulo {
       margin: 0px;
+	  float: left;
+	  margin-right: 20px;
     }
     /* Cloud */ 
     hr.style-two { border: 0; height: 1px; background-image: linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0)); }
@@ -82,8 +85,8 @@
     <h1>Botonera</h1>
     <?php foreach ($archivos as $dir => $files):?>
     <?php natcasesort($files)?>
-		<p class="titulo"><?php echo preg_replace("/[-_]/i"," ",$dir) ?></p>
-		<hr class="style-two"/> 
+		<div class="categoria"><span class="titulo"><?php echo preg_replace("/[-_]/i"," ",$dir) ?></span>
+		
 
 		<?php foreach ($files as $file): ?>
 			<audio id='sonido<?php echo $cnt_a?>' src="<?php echo $sounds_dir.DIRECTORY_SEPARATOR.$dir.DIRECTORY_SEPARATOR.$file?>" preload="true">Tu navegador no soporta el elemento <code>audio</code>.</audio>
@@ -94,7 +97,8 @@
 			<button onclick="document.getElementById('sonido<?php echo $cnt_b?>').play();"><?php echo preg_replace("/[-_]/i"," ",ucfirst($parts['filename']))?></button>
 			<?php $cnt_b++?>
 		<?php endforeach;?>
-
+	       <hr class="style-two" />
+               </ div>
 	<?php endforeach;?>
 	<a href="https://github.com/ftoledo/botonera"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></a>
 
